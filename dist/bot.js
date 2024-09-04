864b6dae7bd19dcb5663b24c33d7896428f56547
Lucy <lucy@amogus.cloud>
2024-09-04 12:33:19 +0000

    remove dist

--- dist/bot.js
+++
@@ -1,7 +0,0 @@
-import { Client } from "discord.js";
-import Enmap from "enmap";
-export class Bot extends Client {
-    counter = new Enmap("counter");
-    reactArray = ["", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"];
-}
-//# sourceMappingURL=bot.js.map
